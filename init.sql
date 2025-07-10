a14b92551976c01f2925fc3890d885441c3e4981
R1der12 <165895573+R1der12@users.noreply.github.com>
2025-07-11 01:09:40 +0300

    Delete init.sql

--- init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "user" (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
-
-INSERT INTO "user" (name) VALUES
-('Maksim'),
-('Andrei Olegovich'),
-('Dmity');
